23bbb2686fc513a832deabb02d216c3e94273c99
ario <cybera.3s@gmail.com>
2021-11-14 17:51:14 +0330

    add: login classmethod > Passenger class

--- metro/models.py
+++ metro/models.py
@@ -62,6 +62,22 @@ class Passenger:
             pickle.dump(self, f)
         return self
 
+    @classmethod
+    def login(cls, username: str, password: str):
+        if os.path.exists('users/users.db'):
+            with open('users/users.db', 'rb') as f:
+                data = pickle.load(f)
+        else:
+            raise LoginError('Username', "Username Doesn't exist")
+
+        if username not in data:
+            raise LoginError('Username', "Username Doesn't exist")
+
+        if data[username]['password'] != password:
+            raise LoginError("Password", "Entered password doesn't match")
+
+        return data[username]['obj']
+
     def __str__(self):
         msg = f"id:{self.id}\nfullname:{self.fullname}\nphone:{self.phone}\nemail:{self.email}"
         return msg
