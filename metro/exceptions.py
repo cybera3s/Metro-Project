3b37aa110cf14df3a8c34a4e0eab9e0670736a9d
ario <cybera.3s@gmail.com>
2021-11-14 20:23:16 +0330

    add BankAccountError exception

--- metro/exceptions.py
+++ metro/exceptions.py
@@ -19,3 +19,14 @@ class LoginError(Exception):
 
     def __str__(self):
         return f"Error due to `{self.reason}` >> {self.msg}"
+
+
+class BankAccountError(Exception):
+
+    def __init__(self, reason, msg, *args) -> None:
+        super().__init__(reason, msg, *args)
+        self.reason = reason
+        self.msg = msg
+
+    def __str__(self):
+        return f"Error due to `{self.reason}` >> {self.msg}"
