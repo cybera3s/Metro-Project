f16aa30abca1dab24f0e20411a3d13f9f7127d3c
ario <cybera.3s@gmail.com>
2021-11-14 14:17:46 +0330

    refactor:main_menu func

---
+++ metro/menus.py
@@ -0,0 +1,27 @@
+def main_menu():
+    while True:
+        print("1. register new user")
+        print("2. manage bank account")
+        print("3. register new trip")
+        print("4. admin panel")
+        print("5. exit")
+
+        option = input(">>> ")
+
+        if option == "1":
+            # todo: user register
+            pass
+        elif option == "2":
+            # todo: manage bank account
+            pass
+        elif option == "3":
+            # todo: new trip
+            pass
+        elif option == "4":
+            # todo: admin panel section
+            pass
+        elif option == "5":
+            # todo: exit
+            break
+        else:
+            print("wrong option, try again")
