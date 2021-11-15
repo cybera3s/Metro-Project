4c358c18c29026fff5d0d7f7ec4398fb67cab68a
ario <cybera.3s@gmail.com>
2021-11-15 14:02:27 +0330

    refactor deposit method > BankAccount

--- metro/models.py
+++ metro/models.py
@@ -147,7 +147,8 @@ class BankAccount:
 
         if not amount.isdigit() or int(amount) < 0:
             raise BankAccountError("deposit", "deposit amount must be a number and greater than zero!")
-        self.__balance += amount
+
+        self.__balance += int(amount)
         self.__owner.save_data()
 
     def get_balance(self):
