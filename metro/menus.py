c2507637bc3f0d2e2ca1704a22f7e249aa78157e
ario <cybera.3s@gmail.com>
2021-11-21 02:15:10 +0330

    refactor utils import

--- metro/menus.py
+++ metro/menus.py
@@ -1,7 +1,6 @@
 from models import Passenger, SingleTrip, CreditCard, TimeCredit, Trip
 from metro.exceptions import RegisterError, BankAccountError, AuthenticationError, TripError, MetroCardError
-from metro.utils import clear_screen, main_menu_options, any_key, manage_bank_account_menu_options, \
-    trip_management_menu_options, buy_cards_menu_options, wrong_option
+from metro.utils import *
 import register_trip
 
 
