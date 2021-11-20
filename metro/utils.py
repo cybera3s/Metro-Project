2ea1b3f287095a3324aa5edb66e12c38f6fb6981
ario <cybera.3s@gmail.com>
2021-11-20 17:38:17 +0330

    refactor clear_screen() func > utils

--- metro/utils.py
+++ metro/utils.py
@@ -1,8 +1,11 @@
 import os
+import time
 
-
-def clear_screen():
+def clear_screen(delay=None):
     """clear the screen in gnu/linux and Microsoft wimdows"""
+    if delay:
+        time.sleep(delay)
+
     if os.name == "posix":
         os.system("clear")
     elif os.name == 'nt':
