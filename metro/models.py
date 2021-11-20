0eb11e3d49271aed378dc4d0f9cce102d7e5b7d3
ario <cybera.3s@gmail.com>
2021-11-20 20:31:47 +0330

    refactor __check_user_data method > Passenger model

--- metro/models.py
+++ metro/models.py
@@ -37,7 +37,7 @@ class Passenger:
     @staticmethod
     def __check_user_data(fullname: str, phone: str, email: str):
         """checks user data and raise registerError"""
-        fullname_regex = r"([a-zA-Z]+)\s{1}([a-zA-Z]+)"
+        fullname_regex = r"^([a-zA-Z]+)\s{1}([a-zA-Z]+)$"
         if not re.match(fullname_regex, fullname):
             msg = "fullname must contain only letters and one space between first name and last name e.g:Sajad Safa"
             raise RegisterError(msg, "fullname", fullname)
