3d0a2bd06490634ccd0e448de1fb43e28311c8c2
ario <cybera.3s@gmail.com>
2021-11-20 19:40:46 +0330

    add main_menu_options func > utils

--- metro/utils.py
+++ metro/utils.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+
 def clear_screen(delay=None):
     """clear the screen in gnu/linux and Microsoft wimdows"""
     if delay:
@@ -10,3 +11,13 @@ def clear_screen(delay=None):
         os.system("clear")
     elif os.name == 'nt':
         os.system("cls")
+
+
+def main_menu_options():
+    """prints main menu options"""
+    print("_____________________ MAIN MENU _____________________\n")
+    print("\t1. register new Passenger")
+    print("\t2. manage bank account")
+    print("\t3. trip management")
+    print("\t4. admin panel")
+    print("\t5. exit")
