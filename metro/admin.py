a3143e1ab74f82e53bcb0134555c48c83dcb5e51
ario <cybera.3s@gmail.com>
2021-11-20 19:16:23 +0330

    refactor admin.py

---
+++ metro/admin.py
@@ -0,0 +1,8 @@
+import sys
+#
+if sys.argv[1] == "createsuperuser":
+      pass
+#     fullname = input("enter your fullname: ")
+#     password = input("enter your password: ")
+#
+# print(fullname, password)
