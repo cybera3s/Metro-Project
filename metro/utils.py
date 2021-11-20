8756352ba5f45107f8f4fac3e845ed4c347af69f
ario <cybera.3s@gmail.com>
2021-11-20 22:01:39 +0330

    add trip_management_menu_options func > utils

--- metro/utils.py
+++ metro/utils.py
@@ -22,6 +22,7 @@ def main_menu_options():
     print("\t4. admin panel")
     print("\t5. exit")
 
+
 def manage_bank_account_menu_options():
     """prints bank account menu optioans"""
     print("_____________________ BANK ACCOUNT MANAGEMENT _____________________\n")
@@ -29,6 +30,14 @@ def manage_bank_account_menu_options():
     print("\t2. withdraw")
     print("\t3. show account balance")
     print("\t4. Back to main menu")
+
+
+def trip_management_menu_options():
+    print("_____________________ TRIP MANAGEMENT MENU _____________________\n")
+    print("\t1. register new trip")
+    print("\t2. buy new card")
+    print("\t3. back to main menu")
+
 
 def any_key():
     key = input("\npress any key to continue...")
