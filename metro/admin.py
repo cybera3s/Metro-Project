701bdf107a6299146864de04ffc4fec598327fe0
ario <cybera.3s@gmail.com>
2021-11-21 17:46:07 +0330

    add create_super_user func > admin

--- metro/admin.py
+++ metro/admin.py
@@ -3,23 +3,34 @@ from models import Admin
 from utils import *
 from exceptions import RegisterError
 
-if sys.argv[1] == "createsuperuser":
-    print("__________________ CREATE ADMIN USER__________________\n")
 
-    fullname = input("enter your fullname: ")
-    password = input("enter your password: ")
+def create_super_user():
+    """register a new admin"""
+    if sys.argv[1] == "createsuperuser":
+        print("__________________ CREATE ADMIN USER__________________\n")
 
-    try:
+        fullname = input("enter your fullname: ")
+        password = input("enter your password: ")
 
-        admin = Admin(fullname, password)
+        try:
+
+            admin = Admin(fullname, password)
+            clear_screen()
+            print(admin.register())
+
+            print("\nsuccessfully registered !")
+            enter_key()
+
+        except RegisterError as e:
+
+            clear_screen()
+            print(e)
+            enter_key()
+
+    else:
+
         clear_screen()
-        print(admin.register())
+        print("wrong PARAMETER !")
 
-        print("\nsuccessfully registered !")
-        enter_key()
 
-    except RegisterError as e:
-
-        clear_screen()
-        print(e)
-        enter_key()
+create_super_user()
