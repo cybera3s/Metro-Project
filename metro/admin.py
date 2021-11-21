4d671f2f2a04463e685614b5079aa5130698152b
ario <cybera.3s@gmail.com>
2021-11-21 17:21:02 +0330

    refactor admin.py script

--- metro/admin.py
+++ metro/admin.py
@@ -1,8 +1,27 @@
 import sys
-#
+from models import *
+from utils import *
+
+
 if sys.argv[1] == "createsuperuser":
-      pass
-#     fullname = input("enter your fullname: ")
-#     password = input("enter your password: ")
-#
-# print(fullname, password)
+    print("__________________ CREATE ADMIN USER__________________\n")
+
+    fullname = input("enter your fullname: ")
+    password = input("enter your password: ")
+
+    try:
+
+        admin = Admin(fullname, password)
+        clear_screen()
+        print(admin.register())
+
+        print("\nsuccessfully registered !")
+        enter_key()
+
+    except RegisterError as e:
+
+        clear_screen()
+        print(e)
+        enter_key()
+
+
