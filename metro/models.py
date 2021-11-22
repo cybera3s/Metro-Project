cdb7bef2259684674f29353b2b7451b4c4c1d8a5
ario <cybera.3s@gmail.com>
2021-11-22 12:56:48 +0330

    refactor __str__ method > TimeCredit model

--- metro/models.py
+++ metro/models.py
@@ -265,7 +265,7 @@ class TimeCredit(MetroCard):
             raise MetroCardError("expire_date", "the card is expired!")
 
     def __str__(self):
-        return f"time-credit (balance: {self.balance} ,{self.remaining_time.days}days to expire)"
+        return f"time-credit (balance: {self.balance} ,{self.remaining_time.days} days to expire)"
 
 
 class Trip:
