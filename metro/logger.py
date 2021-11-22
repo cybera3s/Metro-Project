cd89e6e76585af876b461d6dc0819ff218bd3b9e
ario <cybera.3s@gmail.com>
2021-11-22 16:49:53 +0330

    refactor Logger class > logger.py

--- metro/logger.py
+++ metro/logger.py
@@ -1,10 +1,34 @@
 import logging
 
+fmt = '%(name)s >>> %(levelname)s: %(asctime)s - %(message)s'
+level = logging.DEBUG
+logging.basicConfig(filename='metro.log', format=fmt, level=level)
+
 
 class Logger:
     """logger class for set logs to metro.log"""
-    def __init__(self):
-        self.format = ' %(levelname)s: %(asctime)s - %(message)s'
-        self.level = logging.DEBUG
-        logging.basicConfig(filename='metro.log', format=self.format, level=self.level)
+    logger = None
 
+    @classmethod
+    def set_logger(cls, logger_name):
+        cls.logger = logging.getLogger(logger_name)
+
+    @classmethod
+    def debug(cls, msg):
+        logging.debug(msg)
+
+    @classmethod
+    def info(cls, msg):
+        logging.info(msg)
+
+    @classmethod
+    def warning(cls, msg):
+        logging.warning(msg)
+
+    @classmethod
+    def error(cls, msg):
+        logging.error(msg)
+
+    @classmethod
+    def critical(cls, msg):
+        logging.critical(msg)
