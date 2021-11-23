2e684a74b569e17b3e4fb4851ee17c804a5d4d42
ario <cybera.3s@gmail.com>
2021-11-23 16:20:29 +0330

    create TestPassenger class > tests

--- metro/tests.py
+++ metro/tests.py
@@ -1,2 +1,25 @@
 from models import *
+from exceptions import *
+import unittest
 
+
+class TestPassenger(unittest.TestCase):
+    """test class for Passenger model"""
+
+    def setUp(self) -> None:
+        self.p1 = Passenger("sajad safa", "09224023292", "cybera.3s@gmail.com")
+
+    def test1_fullname_fail(self):
+        self.assertRaises(RegisterError, Passenger, "sajadsafa", "09224023292", "cybera.3s@gmail.com")
+
+    def test2_phoneNumber_fail(self):
+        self.assertRaises(RegisterError, Passenger, "sajad safa", "0922402", "cybera.3s@gmail.com")
+
+    def test3_email_fail(self):
+        self.assertRaises(RegisterError, Passenger, "sajad safa", "09224023292", "cybera.3sgmail.com")
+
+    def test4_obj_success(self):
+        self.assertTrue(self.p1)
+
+    def test_authenticate_fail(self):
+        self.assertRaises(AuthenticationError, Passenger.authenticate, 123456789)
