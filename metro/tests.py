94642bd52c4e5d02c357f80137c00173d137b382
ario <cybera.3s@gmail.com>
2021-11-23 17:16:26 +0330

    create TestAdmin class > tests

--- metro/tests.py
+++ metro/tests.py
@@ -23,3 +23,24 @@ class TestPassenger(unittest.TestCase):
 
     def test_authenticate_fail(self):
         self.assertRaises(AuthenticationError, Passenger.authenticate, 123456789)
+
+
+class TestAdmin(unittest.TestCase):
+    """test class for Admin model"""
+
+    def setUp(self) -> None:
+        self.admin = Admin("sajad safa", "09224023292", "123456789")
+
+    def test_passwords_fail(self):
+        self.assertRaises(RegisterError, Admin, "sajad safa", "09224023292", "1")
+        self.assertRaises(RegisterError, Admin, "sajad safa", "09224023292", "as")
+        self.assertRaises(RegisterError, Admin, "sajad safa", "09224023292", "")
+
+    def test_obj_success(self):
+        self.assertTrue(self.admin)
+
+    def test1_login_fail(self):
+        self.assertRaises(AuthenticationError, Admin.login, 123, "321645")
+
+    def test2_login_fail(self):
+        self.assertRaises(AuthenticationError, Admin.login, 123, "")
